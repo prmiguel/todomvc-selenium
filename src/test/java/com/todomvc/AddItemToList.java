efad64c36175e17fc0ea7ecf3a351a940e252a41
prmiguel <miangel-pr@outlook.com>
2024-01-11 15:59:55 +0000

    Update test

--- src/test/java/com/todomvc/AddItemToList.java
+++ src/test/java/com/todomvc/AddItemToList.java
@@ -1,18 +1,34 @@
 package com.todomvc;
 
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
-import static org.junit.Assert.assertTrue;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
-public class AppTest {
+import static org.junit.Assert.assertEquals;
+
+public class AddItemToList {
 
   @Test
-  public void shouldAnswerWithTrue() {
+  public void shouldAddItemToEmptyList() {
+    String task = "Task 1";
     WebDriver wd = new ChromeDriver();
+    wd.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+    wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     wd.get("https://todomvc.com/examples/angularjs/#/");
+    wd.findElement(By.cssSelector("input.new-todo")).sendKeys(task, Keys.ENTER);
+    Wait<WebDriver> wait = new WebDriverWait(wd, Duration.ofSeconds(5).getSeconds());
+    wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("section.main .todo-list label")));
+    assertEquals(1, wd.findElements(By.cssSelector("section.main .todo-list label")).size());
+    assertEquals(task, wd.findElements(By.cssSelector("section.main .todo-list label")).get(0).getText());
     wd.quit();
-    assertTrue(true);
   }
 }
+
