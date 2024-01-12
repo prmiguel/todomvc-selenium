205028b54b7edd359aac400293c7bf283fa9cff2
prmiguel <miangel-pr@outlook.com>
2024-01-12 16:04:18 +0000

    Add new test

--- src/test/java/com/todomvc/AddItemToList.java
+++ src/test/java/com/todomvc/AddItemToList.java
@@ -10,9 +10,11 @@ import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class AddItemToList {
 
@@ -30,5 +32,21 @@ public class AddItemToList {
     assertEquals(task, wd.findElements(By.cssSelector("section.main .todo-list label")).get(0).getText());
     wd.quit();
   }
+
+  @Test
+	public void shouldAddItemToListWithExistingItems() {
+		String[] existingTasks = { "Task 1", "Task 2", "Task 3" };
+		String newTask = "Task 4";
+		WebDriver wd = new ChromeDriver();
+    wd.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+    wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    wd.get("https://todomvc.com/examples/angularjs/#/");
+		Arrays.asList(existingTasks).stream()
+				.forEach(task -> wd.findElement(By.cssSelector("input.new-todo")).sendKeys(task, Keys.ENTER));
+        wd.findElement(By.cssSelector("input.new-todo")).sendKeys(newTask, Keys.ENTER);
+    assertEquals(4, wd.findElements(By.cssSelector("section.main .todo-list label")).size());
+    assertTrue(wd.findElements(By.cssSelector("section.main .todo-list label")).stream().anyMatch(we -> we.getText().equals(newTask)));
+    wd.quit();
+	}
 }
 
