d70f600b0b8b1bb2b5f2b0cf9ff3142b020d5670
prmiguel <miangel-pr@outlook.com>
2024-01-14 08:00:18 +0000

    Add tests for clearing copleted todos

---
+++ src/test/java/com/todomvc/ClearCompletedTodos.java
@@ -0,0 +1,47 @@
+package com.todomvc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ClearCompletedTodos {
+
+    @Test
+    public void shouldBeAbleToClearCompletedTodos() {
+        String[] existingTasks = { "Task 1", "Task 2" };
+        WebDriver wd = new ChromeDriver();
+        wd.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.get("https://todomvc.com/examples/angularjs/#/");
+        Arrays.asList(existingTasks).stream()
+                .forEach(task -> wd.findElement(By.cssSelector("input.new-todo")).sendKeys(task, Keys.ENTER));
+        wd.findElement(By.xpath("//*[@class='view' and contains(.,'Task 2')]//input[@type='checkbox']")).click();
+        wd.findElement(By.cssSelector(".clear-completed")).click();
+        assertEquals(1, wd.findElements(By.cssSelector("section.main .todo-list label")).size());
+        assertTrue(wd.findElements(By.cssSelector("section.main .todo-list label")).stream()
+                .anyMatch(we -> !we.getText().equals("Task 2")));
+        wd.quit();
+    }
+
+    @Test
+    public void shouldNotBeAbleToClearCompletedTodosIfNoneAreComplete() {
+        String[] existingTasks = { "Task 1", "Task 2" };
+        WebDriver wd = new ChromeDriver();
+        wd.manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        wd.get("https://todomvc.com/examples/angularjs/#/");
+        Arrays.asList(existingTasks).stream()
+                .forEach(task -> wd.findElement(By.cssSelector("input.new-todo")).sendKeys(task, Keys.ENTER));
+        assertEquals(2, wd.findElements(By.cssSelector("section.main .todo-list label")).size());
+        assertTrue(!wd.findElement(By.cssSelector(".clear-completed")).isDisplayed());
+        wd.quit();
+    }
+}
